283688a515bf3583ee4f09a3721941666f228ec1
Rastsislau Matusevich <rostislavmat@gmail.com>
2017-04-19 22:03:33 +0200

    v2.1

--- wybory/elections/views.py
+++ wybory/elections/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .forms import *
 from .models import *
 from django.db import transaction
 import django
-from django.http import HttpResponseRedirect
-from .models import *
-from .utility import *
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
+import os
+import jinja2
+import json
+import random
+from django.db.models import *
+#from django.template.loader import render_to_string
+#import html.parser
+from django.conf import settings
 
 def search(request):
     if request.method == 'POST':
@@ -100,3 +105,72 @@ def change(request, gmina_name):
 
 
 
+
+
+def renderHTML(request , region, title, kraj=False , simple = False):
+    q = region.buildQ()
+    vals = {'region_info': region,
+            'title': title,
+            'data_pie' : buildPieChartData(q),
+            }
+    if simple:
+        vals['change'] = 'change'
+    else:
+            if kraj:
+                vals['data'] = buildMapData()
+            vals['regions_data'] =  (region.subunit().objects.filter(q))
+            vals['unit'] = region.subunit_str;
+    return render(request,'stats.html', vals)
+
+
+
+
+def buildMapData():
+    data = {}
+    cols = [{'label': 'County', 'type': 'string'},
+            {'label': 'Frekwencja', 'type': 'number'}]
+
+    rows = []
+
+    for w in Wojewodztwo.objects.all():
+        turnout = w.valid_votes / w.max_votes * 100
+        vals = []
+        turnout_str = '{0}%'.format(round(turnout, 2))
+        vals.append({'v': w.name, 'f': None})
+        vals.append({'v': turnout, 'f': turnout_str})
+        rows.append({'c': vals})
+
+    data['cols'] = cols
+    data['rows'] = rows
+
+    json_data = json.dumps(data, sort_keys=True, indent=4)
+    return json_data
+
+def getCandidatesAndVotes(q=Q()):
+    candidates = Candidate.objects.all().order_by('name')
+    votes = list(map(lambda c: Vote.objects.filter(q).filter(
+        candidate=c).aggregate(Sum('votes'))['votes__sum'], candidates))
+    total = list(zip(candidates, votes))
+    return total
+
+def buildPieChartData(q=Q()):
+    getCandidatesAndVotes(q)
+
+    data = {}
+    cols = [{'label': 'Kandydat', 'type': 'string'},
+            {'label': 'Liczba głosów', 'type': 'number'}]
+
+    rows = []
+    total = getCandidatesAndVotes(q)
+    for w in total:
+        vals = []
+        turnout_str = '{0}'.format(round(w[1], 2))
+        vals.append({'v': str(w[0]), 'f': None})
+        vals.append({'v': w[1], 'f': turnout_str})
+        rows.append({'c': vals})
+
+    data['cols'] = cols
+    data['rows'] = rows
+
+    json_data = json.dumps(data, sort_keys=True, indent=4)
+    return json_data
